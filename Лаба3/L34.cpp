fa7ceaaf1c38adfddd5504241ac28b2ba83db009
Danya22873 <66486279+Danya22873@users.noreply.github.com>
2023-05-14 22:43:37 +0400

    Add files via upload

---
+++ Лаба3/L34.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void RemoveDuplicates(vector<T>& elements){
+    for (int i = 0; i < elements.size(); ++i) {
+        for (int j = 0; j < elements.size(); ++j) {
+            if(i!=j && elements[i]==elements[j]){
+                elements.erase(elements.begin()+j);
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> v1{6,4,7,6,4,4,0,1};
+    RemoveDuplicates(v1);
+    for (const auto& x : v1) {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    vector<string> v2{"C","C++","C++","C","C++"};
+    RemoveDuplicates(v2);
+    for (const auto& x : v2) {
+        cout<<x<<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}

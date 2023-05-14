8cd0371aa2ce8ca824e9e42b5fbc75463724bd6c
Danya22873 <66486279+Danya22873@users.noreply.github.com>
2023-05-14 22:05:29 +0400

    Add files via upload

---
+++ Лаба3/L32.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+    vector<T> result;
+    for(const auto& elem : elements){
+        if (elem>border){
+            result.push_back(elem);
+        }
+    }
+    return result;
+}
+
+int main() {
+    for(int x: FindGreaterElements(set<int>{1,5,7,8},5)){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+
+    string to_find="Python";
+    cout<< FindGreaterElements(set<string>{"C","C++"}, to_find).size()<<endl;
+    return 0;
+}

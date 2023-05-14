e57610d91197819db2e024ca10ee8541898c60e7
Danya22873 <66486279+Danya22873@users.noreply.github.com>
2023-05-14 22:52:03 +0400

    Add files via upload

---
+++ Лаба3/L35.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print_vector(const vector<int>& v) {
+    for (auto x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        v[i] = i + 1;
+    }
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    do {
+        print_vector(v);
+    } while (prev_permutation(v.begin(), v.end()));
+
+    return 0;
+}
